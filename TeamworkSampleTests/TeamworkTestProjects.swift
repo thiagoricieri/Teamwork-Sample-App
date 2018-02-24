bcb354fe2619d64dd5ca6e6e49dc57857fa8de5d
Thiago Ricieri <thiago.ricieri@gmail.com>
2018-02-24 19:11:26 -0300

    [tests] write Account API tests

---
+++ TeamworkSampleTests/TeamworkTestProjects.swift
@@ -0,0 +1,26 @@
+//
+//  TeamworkTestProjects.swift
+//  TeamworkSampleTests
+//
+//  Created by Thiago Ricieri on 24/02/2018.
+//  Copyright © 2018 Thiago Ricieri. All rights reserved.
+//
+
+import XCTest
+@testable import TeamworkSample
+
+class TeamworkTestProjects: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        
+        
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+}
---
+++ TeamworkSampleTests/TeamworkTestTaskLists.swift
@@ -0,0 +1,26 @@
+//
+//  TeamworkSampleTests.swift
+//  TeamworkSampleTests
+//
+//  Created by Thiago Ricieri on 23/02/2018.
+//  Copyright © 2018 Thiago Ricieri. All rights reserved.
+//
+
+import XCTest
+@testable import TeamworkSample
+
+class TeamworkTestTaskLists: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        
+        
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+}
